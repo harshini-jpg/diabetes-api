353e5cc053b33cc20899dfd36c93fc4e9c487ca7
harshini-jpg <harshinibalakumar2004@gmail.com>
2026-05-21 10:52:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+
+
+from fastapi import FastAPI
+import joblib
+import pandas as pd
+from pydantic import BaseModel
+
+feature_cols = joblib.load('feature_cols.pkl')
+
+model = joblib.load('catboost_diabetes.pkl')
+app = FastAPI()
+
+class DiabetesInput(BaseModel):
+    Pregnancies: int
+    Glucose: float
+    BloodPressure: float
+    SkinThickness: float
+    Insulin: float
+    BMI: float
+    DiabetesPedigreeFunction: float
+    Age: float
+
+@app.get('/')
+
+def home():
+    return {
+        'message':'Diabetes Prediction Running'
+    }
+
+@app.post('/predict')
+
+def predict(data : DiabetesInput):
+    input_df = pd.DataFrame([data])
+    probability = model.predict_proba(input_df)[0][1]
+    prediction = int(probability >= 0.5)
+    return {
+        'prediction':prediction,
+        'diabetes_probability':probability
+    }
